5303ae698d053d046b554f1d90a05fcdac8d3c3c
v-deshet <v-deshet@MININT-V6DK8MH>
2018-03-01 15:07:31 -0800

    Add project files.

---
+++ test3/Test3Form.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test3
+{
+    public partial class Test3Form : Form
+    {
+        public Test3Form()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //test
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
